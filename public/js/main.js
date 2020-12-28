307b13622b6b4090a12b1b81d49511f4da07e5cb
Malo <ndekezi37@gmail.com>
2020-12-28 13:49:24 +0100

    moitier de l'exercice

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+let phase = "J'aime les tartines et j'en mange"
+console.log(pahse)
+
+let phase2 = "4 par jours"
+
+let phase3 = pahse + " " + phase2
+
+console.log(phase3)
+
+let phase4 = 5
+let phase5 = 35
+
+let phase6 = phase4 + phase5
+console.log(phase6)
+
+let phase7 = phase1 + " " + phase4
