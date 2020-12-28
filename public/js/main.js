f01a4fc30e54f70546f602f5245b4229a27896dc
Malo <ndekezi37@gmail.com>
2020-12-28 14:48:41 +0100

    fin de l'exo

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,41 @@
-let phase = "J'aime les tartines et j'en mange"
-console.log(pahse)
+// let phrase = "J'aime les tartines et j'en mange";
+// console.log(pahse);
+// let phrase2 = "4 par jours";
+// let result = phrase + " " + phase2;
+// console.log(result);
 
-let phase2 = "4 par jours"
 
-let phase3 = pahse + " " + phase2
+// let nombre = 5;
+// let nombre2 = 35;
 
-console.log(phase3)
+// console.log(nombre + nombre2);
+// console.log(phrase + nombre);
+// concatener
 
-let phase4 = 5
-let phase5 = 35
+// fin exo 1
 
-let phase6 = phase4 + phase5
-console.log(phase6)
 
-let phase7 = phase1 + " " + phase4
+// reafectation
+
+let salut = "salut tout le monde";
+console.log(salut);
+let arv = "aurevoir !";
+console.log(arv);
+
+salut = arv;
+console.log(salut);
+// fin exo 3
+
+let boo1 = 4;
+let boo2 = "6";
+
+// ope & booleans
+let num1 = 4;
+let num2 = "6";
+``
+conosole.log(num1 > num2);
+conosole.log(num1 < num2);
+conosole.log(num1 == num2);
+conosole.log(num1 != num2);
+conosole.log(num1 === num2);
+conosole.log(num1 !== num2);
